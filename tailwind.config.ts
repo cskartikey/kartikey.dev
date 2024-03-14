5fb404c31b23b5c4c976ad081042bb51c8dcf965
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-03-15 02:13:25 +0530

    Inital Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "background-black": "#0F0F0F",
+        "accent-green": "#008170",
+      },
+      fontFamily: {
+        playfair: ["var(--font-playfair)"],
+        poppins: ["var(--font-poppins)"],
       },
     },
   },
